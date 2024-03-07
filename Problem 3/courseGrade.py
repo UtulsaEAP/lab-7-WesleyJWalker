d910adda8f53d52df59f93e07f9d6d367f1f22df
Wesley <wesleyjameswalker@gmail.com>
2024-03-07 13:00:45 -0600

    lab7

--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -1,60 +1,50 @@
 def courseGrade():
     
     # TODO: Declare any necessary variables here. 
-    allInfo = []
-    allReports = []
-    fileNames = input().split("\n")
-    tableNum = len(fileNames)
-    studentInfo = open(fileNames[0], 'r')
-    allInfo.append(studentInfo)
-    report = open("./Problem 3/report.txt", 'a')
-    allReports.append(report)
-    if tableNum >= 2:
-        studentInfo1 = open(fileNames[1], 'r')
-        allInfo.append(studentInfo1)
-        report1 = open("./Problem 3/report1.txt", 'a')
-        allReports.append(report1)
-    if tableNum == 3:
-        studentInfo2 = open(fileNames[2], 'r')
-        allInfo.append(studentInfo1)
-        report2 = open("./Problem 3/report2.txt", 'a')
-        allReports.append(report2)
+    fileName = input()
+    studentInfo = open(fileName, 'r')
+    if fileName == "./Problem 3/StudentInfo.tsv":
+        report = open("./Problem 3/report.txt", 'a')
+    elif fileName == "./Problem 3/StudentInfo1.tsv":
+        report = open("./Problem 3/report1.txt", 'a')
+    elif fileName == "/Problem 3/StudentInfo2.tsv":
+        report = open("./Problem 3/report2.txt", 'a')
+    else:
+        print("Invalid file name")
+        return
     
-    i = 0
-    while i < tableNum:
-        count = 0
-        midterm1Av = 0
-        midterm2Av = 0
-        finalAv = 0
-        for line in allInfo[i]:
-            infolist = line.split()
-            count += 1
-            midterm1Av += float(infolist[2])
-            midterm2Av += float(infolist[3])
-            finalAv += float(infolist[4])
-            if ((float(infolist[2]) + float(infolist[3]) + float(infolist[4])) / 3) >= 90:
-                grade = "A"
-                infolist.append(grade)
-            elif ((float(infolist[2]) + float(infolist[3]) + float(infolist[4])) / 3) >= 80:
-                grade = "B"
-                infolist.append(grade)
-            elif ((float(infolist[2]) + float(infolist[3]) + float(infolist[4])) / 3) >= 70:
-                grade = "C"
-                infolist.append(grade)
-            elif ((float(infolist[2]) + float(infolist[3]) + float(infolist[4])) / 3) >= 60:
-                grade = "D"
-                infolist.append(grade)
-            else:
-                grade = "F"
-                infolist.append(grade)
-            for j in range(6):
-                allReports[i].write(str(infolist[j]) + " ")
-            allReports[i].write("\n")
-        midterm1Av = midterm1Av / count
-        midterm2Av = midterm2Av / count
-        finalAv = finalAv / count
-        allReports[i].write("Averages: midterm1 " + f'{midterm1Av:.2f}' + ", midterm2 " + f'{midterm2Av:.2f}' + ", final " + f'{finalAv:.2f}')
-        i += 1
+    count = 0
+    midterm1Av = 0
+    midterm2Av = 0
+    finalAv = 0
+    for line in studentInfo:
+        infolist = line.split()
+        count += 1
+        midterm1Av += float(infolist[2])
+        midterm2Av += float(infolist[3])
+        finalAv += float(infolist[4])
+        if ((float(infolist[2]) + float(infolist[3]) + float(infolist[4])) / 3) >= 90:
+            grade = "A"
+            infolist.append(grade)
+        elif ((float(infolist[2]) + float(infolist[3]) + float(infolist[4])) / 3) >= 80:
+            grade = "B"
+            infolist.append(grade)
+        elif ((float(infolist[2]) + float(infolist[3]) + float(infolist[4])) / 3) >= 70:
+            grade = "C"
+            infolist.append(grade)
+        elif ((float(infolist[2]) + float(infolist[3]) + float(infolist[4])) / 3) >= 60:
+            grade = "D"
+            infolist.append(grade)
+        else:
+            grade = "F"
+            infolist.append(grade)
+        for j in range(6):
+            report.write(str(infolist[j]) + " ")
+        report.write("\n")
+    midterm1Av = midterm1Av / count
+    midterm2Av = midterm2Av / count
+    finalAv = finalAv / count
+    report.write("Averages: midterm1 " + f'{midterm1Av:.2f}' + ", midterm2 " + f'{midterm2Av:.2f}' + ", final " + f'{finalAv:.2f}')
       
       
     # TODO: Read a file name from the user and read the tsv file here. 
