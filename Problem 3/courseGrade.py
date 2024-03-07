70e9618a56102d0c927626c2522df55cc7f2bb88
Wesley <wesleyjameswalker@gmail.com>
2024-03-07 12:38:08 -0600

    lab7

--- Problem 3/courseGrade.py
+++ Problem 3/courseGrade.py
@@ -3,19 +3,19 @@ def courseGrade():
     # TODO: Declare any necessary variables here. 
     allInfo = []
     allReports = []
-    fileNames = input().strip()
+    fileNames = input().split("\n")
     tableNum = len(fileNames)
-    studentInfo = open("./Problem 3/StudentInfo.tsv", 'r')
+    studentInfo = open(fileNames[0], 'r')
     allInfo.append(studentInfo)
     report = open("report.txt", 'a')
     allReports.append(report)
     if tableNum >= 2:
-        studentInfo1 = open("./Problem 3/StudentInfo1.tsv", 'r')
+        studentInfo1 = open(fileNames[1], 'r')
         allInfo.append(studentInfo1)
         report1 = open("report1.txt", 'a')
         allReports.append(report1)
     if tableNum == 3:
-        studentInfo2 = open("./Problem 3/StudentInfo.tsv", 'r')
+        studentInfo2 = open(fileNames[2], 'r')
         allInfo.append(studentInfo1)
         report2 = open("report2.txt", 'a')
         allReports.append(report2)
@@ -27,34 +27,34 @@ def courseGrade():
         midterm2Av = 0
         finalAv = 0
         for line in allInfo[i]:
-            infolist = line.strip(" ")
-            print(infolist[2])
-            '''count += 1
+            infolist = line.split()
+            count += 1
             midterm1Av += float(infolist[2])
             midterm2Av += float(infolist[3])
             finalAv += float(infolist[4])
-            if (infolist[2] + infolist[3] + infolist[4]) / 3 >= 90:
+            if ((float(infolist[2]) + float(infolist[3]) + float(infolist[4])) / 3) >= 90:
                 grade = "A"
-                infolist.append[grade]
-            elif (infolist[2] + infolist[3] + infolist[4]) / 3 >= 80:
+                infolist.append(grade)
+            elif ((float(infolist[2]) + float(infolist[3]) + float(infolist[4])) / 3) >= 80:
                 grade = "B"
-                infolist.append[grade]
-            elif (infolist[2] + infolist[3] + infolist[4]) / 3 >= 70:
+                infolist.append(grade)
+            elif ((float(infolist[2]) + float(infolist[3]) + float(infolist[4])) / 3) >= 70:
                 grade = "C"
-                infolist.append[grade]
-            elif (infolist[2] + infolist[3] + infolist[4]) / 3 >= 60:
+                infolist.append(grade)
+            elif ((float(infolist[2]) + float(infolist[3]) + float(infolist[4])) / 3) >= 60:
                 grade = "D"
-                infolist.append[grade]
+                infolist.append(grade)
             else:
                 grade = "F"
-                infolist.append[grade]
+                infolist.append(grade)
             for j in range(6):
                 allReports[i].write(str(infolist[j]) + " ")
-                allReports[i].write("\n")
+            allReports[i].write("\n")
         midterm1Av = midterm1Av / count
         midterm2Av = midterm2Av / count
         finalAv = finalAv / count
-        allReports[i].write("Averages: midterm1 " + f'{midterm1Av:.2f}' + ", midterm2 " + f'{midterm2Av:.2f}' + ", final " + f'{finalAv:.2f}')'''
+        allReports[i].write("Averages: midterm1 " + f'{midterm1Av:.2f}' + ", midterm2 " + f'{midterm2Av:.2f}' + ", final " + f'{finalAv:.2f}')
+        i += 1
       
       
     # TODO: Read a file name from the user and read the tsv file here. 
--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -1,6 +1,9 @@
 def fileNameChange():
     #type your code here
-
+    with open(input(), '+r') as ParkPhotos:
+        for line in ParkPhotos:
+            toprint = line.replace("_photo.jpg", "_info.txt")
+            print(toprint.strip("\n"))
     return
 
 if __name__ == '__main__':
